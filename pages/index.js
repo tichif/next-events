cfaf01543b5363bdc2a8663cc72bd41cd4d787a7
Charles-Hebert Dalzon <charlby5@gmail.com>
2021-07-26 13:08:37 -0400

    Configure the head section

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import Head from 'next/head';
+
 import { getFeaturedEvents } from '../dummy-data';
 import EventList from '../components/events/EventList';
 
@@ -6,6 +8,13 @@ const HomePage = () => {
 
   return (
     <div>
+      <Head>
+        <title>NextJS Events</title>
+        <meta
+          name='description'
+          content='Find a lot of great events that allows you evolve...'
+        />
+      </Head>
       <EventList items={featuredEvents} />
     </div>
   );
